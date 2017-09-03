42452569f74853baece3cd1ec134378262c04e2e
veysel <vinanc72@gmail.com>
2017-09-03 23:14:51 +0300

    Home service added. HttpModule added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './Components/Home/home.component';
+
+import { HomeService } from './Components/Home/Services/home.service';
 
 @NgModule({
   declarations: [
@@ -10,9 +13,12 @@ import { HomeComponent } from './Components/Home/home.component';
     HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule
   ],
-  providers: [],
+  providers: [
+    HomeService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
