4d1976ae8f4730301bd11f4abbf568c16d39fc61
veysel <vinanc72@gmail.com>
2017-09-03 14:44:57 +0300

    Schema created to project

--- src/app/Components/Home/home.component.ts
+++ src/app/Components/Home/home.component.ts
@@ -1,9 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { ViewTypeModel } from './Models/view.type.model';
+
+import { ViewTypeEnum } from './Enums/view.type.enum';
 
 @Component({
     selector: 'home',
     templateUrl: 'home.template.html'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+
+    private viewTypeOptions: ViewTypeModel = new ViewTypeModel();
+
+    ngOnInit() {
+        this.SetViewTypeOptions(ViewTypeEnum.ViewMainList)
+    }
+
+    private SetViewTypeOptions(viewType: ViewTypeEnum) {
+        // For Make False All Property.
+        this.viewTypeOptions = new ViewTypeModel();
+
+        switch (viewType) {
+            case ViewTypeEnum.ViewMainList: {
+                this.viewTypeOptions.ViewMainList = true;
+                break;
+            }
+            case ViewTypeEnum.ViewPodcastList: {
+                this.viewTypeOptions.ViewPodcastList = true;
+                break;
+            }
+            case ViewTypeEnum.ViewDetails: {
+                this.viewTypeOptions.ViewDetails = true;
+                break;
+            }
+            default: {
+                break;
+            }
+        }
+    }
 
 }
